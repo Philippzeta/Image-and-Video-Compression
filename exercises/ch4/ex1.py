240d53b5008bb30b4b62cad533518dba5338272e
Wang <93618264+Philippzeta@users.noreply.github.com>
2025-01-07 15:04:13 +0100

    1

--- exercises/ch4/ex1.py
+++ exercises/ch4/ex1.py
@@ -1,6 +1,6 @@
 import numpy as np
 from ivclab.image import IntraCodec
-
+from ivclab.utils import imread, calc_psnr
 # Implement the IntraCodec and all the necessary modules
 # Use lena_small to train Huffman coder of IntraCodec.
 # Use it to compress and decompress 'lena.tif'
